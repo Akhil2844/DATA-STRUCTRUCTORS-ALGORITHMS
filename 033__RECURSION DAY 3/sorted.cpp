8c6d2f8c9212c833c6c042955fe03d0eab335811
akhil <21bch075@nith.ac.in>
2023-08-14 15:49:21 +0530

    add

---
+++ 033__RECURSION DAY 3/sorted.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int*arr ,int n)
+{
+    // base case
+    if(n==0||n==1)
+    {
+        return true;
+    }
+
+    if(arr[0]>arr[1])
+    {
+        return false;
+    }
+
+    else
+    {
+        bool remainingPart=isSorted(arr+1,n-1);
+        return remainingPart;
+    }
+}
+int main()
+{
+
+    int arr[5]={1,4,3,5,8};
+    
+    int ans=isSorted(arr,5);
+
+    if(ans)
+    {
+        cout<<"Array is Sorted "<<endl;
+    }
+    else
+    {
+        cout<<"Array is not Sorted "<<endl;
+    }
+
+   return 0;
+
+}
