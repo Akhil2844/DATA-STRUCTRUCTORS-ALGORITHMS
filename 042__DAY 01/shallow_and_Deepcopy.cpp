d1f27b1ca8a5619090af513f925577de9a9daa27
akhil <21bch075@nith.ac.in>
2023-08-18 23:23:45 +0530

    add

---
+++ 042__DAY 01/shallow_and_Deepcopy.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+class Hero
+{
+
+
+    private:
+    int health;
+
+    public:
+    char *name;
+    char level;
+
+    // constructor called
+    Hero()
+{
+    cout<<"Simple Constructor Called "<<endl;
+    name=new char[100];
+}
+
+
+
+
+
+
+// // copy constructor
+Hero(Hero& temp)
+{
+    // deep copy
+    cout<<"Copy constructor called "<<endl;
+    
+
+     char*ch=new char[100];
+     strcpy(ch,temp.name);
+     this->name=ch;
+
+    this->health=temp.health;
+    this->level=temp.level;
+}
+
+
+
+
+    void print()
+    {
+        cout<<endl;
+        cout<<"[ Name : "<<this->name<<" ,";
+        cout<<" Health : "<<this->health<<" ,";
+        cout<<" Level : "<<this->level<<" ] ";
+        cout<<endl;
+        cout<<endl;
+    }    
+
+    //getters
+    int getHealth()
+    {
+        return health;
+    }
+
+    char getLevel()
+    {
+        return level;
+    } 
+
+    // setters
+    void setHealth(int h)
+    {
+        health=h;
+    }
+
+    void setLevel(char l)
+    {
+       level=l;
+    }
+    
+    void setName(char name[])
+    {
+      strcpy(this->name,name);
+    }
+    };
+
+  
+
+int main()
+{
+  Hero hero1;
+  hero1.setHealth(70);
+  hero1.setLevel('A');
+
+  char name[7]="Akhilk";
+  hero1.setName(name);
+  hero1.print();
+
+
+//   default copy constructor 
+Hero hero2(hero1);
+// Hero hero2=hero1;
+
+hero2.print();
+
+hero1.name[0]='G';
+
+hero1.print();
+hero2.print();
+
+  
+
+    return 0;
+
+}
