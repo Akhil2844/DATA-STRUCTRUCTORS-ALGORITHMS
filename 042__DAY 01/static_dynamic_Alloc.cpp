f5d20a355623887deabdcab320ae95563570b4e4
akhil <21bch075@nith.ac.in>
2023-08-18 19:47:39 +0530

    add

--- 042__DAY 01/static_dynamic_Alloc.cpp
+++ 042__DAY 01/static_dynamic_Alloc.cpp
@@ -1,2 +1,66 @@
 #include<iostream>
 using namespace std;
+class Hero
+{
+
+    private:
+    int health;
+
+    public:
+    char level;
+
+    void print ()
+    {
+        // cout<<level<<endl;
+    }    
+
+    //getters
+    int getHealth()
+    {
+        return health;
+    }
+
+    char getLevel()
+    {
+        return level;
+    } 
+
+    // setters
+    void setHealth(int h)
+    {
+        health=h;
+    }
+
+    void setLevel(char l)
+    {
+       level=l;
+    }
+    
+    };
+
+
+int main()
+{
+   // Static Allocation
+    Hero a;
+    a.setHealth(90);
+    a.setLevel('B');
+    cout<<"Health is : "<<a.getHealth()<<endl;
+    cout<<"Level is : "<<a.getLevel()<<endl;
+
+
+   // Dynamic Memory Allocation
+   Hero*b=new Hero;
+   b->setHealth(70);
+   b->setLevel('T');
+   cout<<"Health is : "<<(*b).getHealth()<<endl;
+   cout<<"Level is : "<<(*b).getLevel()<<endl;
+
+   cout<<"Health is : "<<b->getHealth()<<endl;
+   cout<<"Level is : "<<b->getLevel()<<endl;
+
+
+    return 0;
+
+}
+
