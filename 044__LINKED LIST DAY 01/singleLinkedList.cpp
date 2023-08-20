aaead2ae0ad89fecc7214b83d6e15a708fdc2501
akhil <21bch075@nith.ac.in>
2023-08-20 23:43:31 +0530

    add

---
+++ 044__LINKED LIST DAY 01/singleLinkedList.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    
+    // constructor
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+
+
+
+};
+
+
+
+
+void InsertAtTail(Node* &tail,int d)
+{
+    // new tail create
+    Node*temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+}
+
+void InsertAtHead(Node* &head,int d )
+{
+    // new node create
+    Node*temp=new Node(d);
+    temp->next=head;
+    head=temp;
+
+
+}
+
+void print(Node*&head)
+{
+    Node*temp=head;
+   while(temp!=NULL)
+   {
+   cout<<temp->data<<" ";
+   temp=temp->next;
+   }    
+   cout<<endl;
+}
+
+void InsertAtPosition(Node* &tail,Node* &head,int position,int d)
+{
+
+    // insert at start
+
+if(position==1)
+{
+    InsertAtHead(head,d);
+    return;
+}
+
+
+    Node*temp=head;
+    int cnt=1;
+
+    while(cnt<position-1)
+    {
+    temp=temp->next;
+    cnt++;
+    }
+
+  // creating a node for d
+    Node*NodeToInsert=new Node(d);
+        NodeToInsert->next=temp->next;
+        temp->next=NodeToInsert;
+
+
+// insert at end 
+if(temp->next==NULL)
+{
+    InsertAtTail(tail,d);
+    return;
+}
+}
+
+int main()
+{
+
+    // created a new Node
+    Node*node1=new Node(10);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+
+    // head pointed to node 1
+
+    Node*head=node1;
+    Node*tail=node1;
+
+    // print(head);
+    print(head);
+
+    // InsertAtHead(head,12);
+    InsertAtTail(tail,12);
+
+
+    // print(head);
+    print(head);
+
+    // InsertAtHead(head,15);
+    InsertAtTail(tail,15);
+
+
+    // print(head);
+    print(head);
+
+
+    InsertAtPosition(tail,head,3,22);
+    print(head);
+    return 0;
+}
