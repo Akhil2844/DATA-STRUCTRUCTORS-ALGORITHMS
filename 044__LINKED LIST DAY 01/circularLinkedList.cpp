cd5dc677f8b4895f25a04327d94f091288e85493
akhil <21bch075@nith.ac.in>
2023-08-26 19:48:09 +0530

    add

---
+++ 044__LINKED LIST DAY 01/circularLinkedList.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node*next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+
+    // DESTRUCTOR
+    ~Node()
+{
+    int value=this->data;
+    if(this->next!=NULL)
+    {
+        delete next;
+        this->next=NULL;
+    }
+ cout<<"Memory free for this node at this data is "<<value<<endl;
+}
+};
+
+void InsertatNode(Node*&tail,int element,int d)
+{
+
+    // empty list
+    if(tail==NULL)
+    {
+        Node* newNode=new Node(d);
+        tail=newNode;
+        newNode->next=newNode;
+
+    }
+
+    else
+    {
+      // non empty list
+    //   assuming that the element is present in the last
+       
+       Node*curr=tail;
+       while(curr->data!=element)
+       {
+        curr=curr->next;
+
+       }
+       
+
+       // element found -> currr is responding element wala node
+       Node*temp=new Node(d);
+       temp->next=curr->next;
+       curr->next=temp;
+
+    }
+}
+ 
+void print(Node* tail)
+{
+  Node*temp=tail;
+
+    if(tail==NULL)
+    {
+        cout<<"List is empty "<<endl;
+    }
+  do
+  {
+    cout<<tail->data<<" ";
+    tail=tail->next;
+  } while (tail!=temp);
+  cout<<endl;
+
+}
+
+// deletion
+void DeleteNode(Node*&tail,int value)
+{
+    // empty list
+    if(tail==NULL)
+    {
+        cout<<"List is empty,please check again "<<endl;
+        return;
+    }
+
+    else
+    {
+        // non empty
+
+        // assuming that the value is present in the linked list
+        Node*prev=tail;
+        Node*curr=prev->next;
+
+        while(curr->data != value)
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+
+        prev->next=curr->next;
+
+        // 1 NODE LINKED LIST
+        if(curr==prev)
+        {
+            tail=NULL;
+        }
+
+        // 2 NODE LINKED LIST
+        if(tail==curr)
+        {
+            tail=prev;
+        }
+        curr->next=NULL;
+        delete curr;
+
+
+    }
+}
+
+
+int main()
+{
+
+    Node*tail=NULL;
+
+    InsertatNode(tail,5,3);
+    print(tail);
+
+    // InsertatNode(tail,3,9);
+    // print(tail);
+
+    // InsertatNode(tail,9,11);
+    // print(tail);
+
+    // InsertatNode(tail,3,6);
+    // print(tail);
+
+    // InsertatNode(tail,6,15);
+    // print(tail);
+
+    DeleteNode(tail,3);
+    print(tail);
+    return 0;
+}
