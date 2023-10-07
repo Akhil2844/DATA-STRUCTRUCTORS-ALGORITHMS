ac756cf3ae34fcbd30b171ef80173d1366d72a11
akhil <21bch075@nith.ac.in>
2023-10-07 22:44:14 +0530

    add

--- 078__BINARY SEARCH TREES DAY 01/intro.cpp
+++ 078__BINARY SEARCH TREES DAY 01/intro.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<queue>
+#include <queue>
 using namespace std;
 
 class Node
@@ -18,141 +18,137 @@ public:
     }
 };
 
-Node* insertIntoBST(Node*root,int d)
+Node *insertIntoBST(Node *root, int d)
 {
     // base case
-    if(root==NULL)
+    if (root == NULL)
     {
-        root=new Node(d);
+        root = new Node(d);
         return root;
     }
 
-    if(d>root->data)
+    if (d > root->data)
     {
-      root->right=insertIntoBST(root->right,d);
+        root->right = insertIntoBST(root->right, d);
     }
     else
     {
-        root->left=insertIntoBST(root->left,d);
+        root->left = insertIntoBST(root->left, d);
     }
 
     return root;
 }
 
-
-void takeInput(Node* &root)
+void takeInput(Node *&root)
 {
     int data;
-    cin>>data;
+    cin >> data;
 
-    while(data!=-1)
+    while (data != -1)
     {
-        root=insertIntoBST(root,data);
-        cin>>data;
-        
+        root = insertIntoBST(root, data);
+        cin >> data;
     }
 }
 
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
 
- void levelOrderTraversal(Node*root)
- {
-    queue<Node*>q;
-   q.push(root);
-    
     q.push(NULL);
 
-while(!q.empty())
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl; // purana level complete ho chuka h
+            if (!q.empty())
+            {
+                // queue still have some child Nodes
+                q.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void inorder(Node *root)
 {
-      Node*temp=q.front();
-      q.pop();
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
 
-      if(temp==NULL)
-      {
-        cout<<endl;  // purana level complete ho chuka h
-        if(!q.empty())
-      {
-        // queue still have some child Nodes
-        q.push(NULL);
-      }
-      }
-
-      else
-      {
-        cout<<temp->data<<" ";
-        if(temp->left)
-        {
-            q.push(temp->left);
-        }
-        if(temp->right)
-        {
-            q.push(temp->right);
-        }
-      }
-
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
 }
+
+void preorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
 }
-   
-    void inorder(Node*root)
-    {
-        // base case
-        if(root==NULL)
-        {
-            return;
-        }
 
-        inorder(root->left);
-        cout<<root->data<<" ";
-        inorder(root->right);
+void postorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
     }
 
-    void preorder(Node*root)
-    {
-        // base case
-        if(root==NULL)
-        {
-            return;
-        }
-
-        cout<<root->data<<" ";
-        preorder(root->left);
-        preorder(root->right);
-    }
-
-    void postorder(Node*root)
-    {
-        // base case
-        if(root==NULL)
-        {
-            return;
-        }
-
-        postorder(root->left);
-        postorder(root->right);
-        cout<<root->data<<" ";
-    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
 
 int main()
 {
 
-    Node*root=NULL;
+    Node *root = NULL;
 
-    cout<<"Enter data to create BST : "<<endl
-    
-    ;
-    
+    cout << "Enter data to create BST : " << endl
+
+        ;
+
     takeInput(root);
 
     levelOrderTraversal(root);
 
-    cout<<"Inorder Traversal is : "<<endl;
+    cout << "Inorder Traversal is : " << endl;
     inorder(root);
-    cout<<endl;
+    cout << endl;
 
-    cout<<"preorder Traversal is : "<<endl;
+    cout << "preorder Traversal is : " << endl;
     preorder(root);
-    cout<<endl;
+    cout << endl;
 
-    cout<<"Postorder Traversal is : "<<endl;
+    cout << "Postorder Traversal is : " << endl;
     postorder(root);
 
     return 0;
---
+++ 078__BINARY SEARCH TREES DAY 01/searchINBST.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+class BinaryTreeNode
+{
+public:
+    int data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    // constructor
+    BinaryTreeNode(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+bool searchInBST(BinaryTreeNode *root,int x)
+{
+
+    // ********  APPROCH 1***********
+// base case
+/*if(root==NULL)
+{
+    return false;
+}
+
+if(root->data==x)
+{
+    return true;
+
+}
+if(root->data>x)
+{
+    searchInBST(root->left,x);
+}
+else
+{
+    searchInBST(root->right,x);
+}*/
+
+
+//***********APPROCH 2**********
+// base case
+BinaryTreeNode*temp=root;
+while(temp!=NULL)
+{
+    if(temp->data==x)
+    {
+        return true;
+    }
+    if(temp->data>x)
+    {
+        temp=temp->left;
+    }
+    else
+    {
+        temp=temp->right;
+    }
+}
+
+
+
+}
+
+int main() {
+    // Creating a binary search tree
+    BinaryTreeNode* root = new BinaryTreeNode(10);
+    root->left = new BinaryTreeNode(5);
+    root->right = new BinaryTreeNode(15);
+    root->left->left = new BinaryTreeNode(3);
+    root->left->right = new BinaryTreeNode(7);
+
+    // Element to search for
+    int x = 7;
+
+    // Call the searchInBST function to search for x in the BST
+    bool found = searchInBST(root, x);
+
+    // Check if the element was found
+    if (found) {
+        cout << x << " found in the BST." << endl;
+    } else {
+        cout << x << " not found in the BST." << endl;
+    }
+
+    // Clean up memory (assuming you have a function for this)
+    // Remember to delete the nodes if they were dynamically allocated
+
+    return 0;
+}
